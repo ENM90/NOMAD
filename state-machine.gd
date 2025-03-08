1471f35cc6aff0c1fd8d5488bd312191db2bc24d
ENM90 <StylusGD@gmail.com>
2025-03-08 19:24:03 -0300

    Create state-machine.gd

---
+++ state-machine.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var player: CharacterBody3D
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(delta: float) -> void:
+	pass
+
+func physics_update(delta: float) -> void:
+	pass
